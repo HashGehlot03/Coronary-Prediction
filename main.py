af432f9c874fc2a921f9da9cfe6d39216f4abdf0
Harish Gehlot <86105660+HashGehlot03@users.noreply.github.com>
2021-11-10 12:10:01 +0530

    upd4

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ cols = [col for col in df.columns if col != 'TenYearCHD']
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods = ['POST'])
+@app.route('/predict',methods = ['GET','POST'])
 def predict():
     int_features = [float(x) for x in request.form.values()]
 
